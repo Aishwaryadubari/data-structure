9f6a9c865ddc9164fc599bb8e06999032f20f133
Aishwarya D <60175634+Aishwaryadubari@users.noreply.github.com>
2022-09-02 12:03:02 +0530

    stack

--- Main.java
+++ Main.java
@@ -1,11 +1,20 @@
+package stacks;
 
 public class Main {
-	public static void main(String[] args)
-	{
-		LinkedList list = new LinkedList(1);
-		list.append(2);
-		list.append(7);
-		
-			
-	}
+public static void main (String[] args)
+{
+	Stack mystack = new Stack(2);
+	mystack.push(1);
+	mystack.push(8);
+	mystack.push(9);
+	mystack.pop();
+	mystack.printStack();
+	
+	Queue myqueue = new Queue(1);
+	myqueue.enqueue(4);
+	myqueue.enqueue(8);
+	myqueue.enqueue(5);
+	myqueue.dequeue();
+	
+}
 }
